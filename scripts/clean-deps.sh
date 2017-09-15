0176765e139d95c27b90baa114d7cf7f0a440cd8
cameron <cameronrll@aol.com>
2017-09-15 17:14:49 +0100

    updated scripts

--- scripts/clean-deps.sh
+++ scripts/clean-deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd /apps/test-node-deploy
+cd /apps/test-docker-ci-cd-deploy
 rm -rf node_modules
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 supervisorctl stop test-node-deploy
-rm /etc/supervisor/conf.d/chs-server.conf
+rm /etc/supervisor/conf.d/test-node-deploy.conf
 
 supervisorctl reread
 supervisorctl update test-node-deploy
