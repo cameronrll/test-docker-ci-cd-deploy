14ca18a381d04f79b2a9c04be4e17b404004c38a
cameron <cameronrll@aol.com>
2017-09-19 11:06:00 +0100

    enable codedeploy

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-echo "would totally deploy"
+export AWS_CODE_DEPLOY_REGION=eu-west-2
+export AWS_CODE_DEPLOY_APPLICATION_NAME="CamsNode"
+export AWS_CODE_DEPLOY_DEPLOYMENT_CONFIG_NAME=CodeDeployDefault.OneAtATime
+export AWS_CODE_DEPLOY_DEPLOYMENT_GROUP_NAME="MasterNode"
+export AWS_CODE_DEPLOY_APP_SOURCE=$HOME/test-docker-ci-cd-deploy
+export AWS_CODE_DEPLOY_S3_FILENAME="cams-node-deploy-${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}"
+export AWS_CODE_DEPLOY_S3_BUCKET=cams-node-deploy
+export AWS_CODE_DEPLOY_S3_LIMIT_BUCKET_FILES=10
+export AWS_CODE_DEPLOY_REVISION_DESCRIPTION="Deployed from ${CIRCLE_BRANCH} with build number ${CIRCLE_BUILD_NUM}"
+export AWS_CODE_DEPLOY_DEPLOYMENT_DESCRIPTION="Deployed via CircleCI on $(date)"
+export AWS_CODE_DEPLOY_S3_KEY_PREFIX="cams-node-deploy"
 
-#export AWS_CODE_DEPLOY_REGION=eu-west-2
-#export AWS_CODE_DEPLOY_APPLICATION_NAME="testNodeDeploy"
-#export AWS_CODE_DEPLOY_DEPLOYMENT_CONFIG_NAME=CodeDeployDefault.AllAtOnce
-#export AWS_CODE_DEPLOY_DEPLOYMENT_GROUP_NAME="testNodeDevelopment"
-#export AWS_CODE_DEPLOY_APP_SOURCE=$HOME/test-docker-ci-cd-deploy
-#export AWS_CODE_DEPLOY_S3_FILENAME="test-node-deploy-${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}"
-#export AWS_CODE_DEPLOY_S3_BUCKET=test-node-deploy-builds
-#export AWS_CODE_DEPLOY_S3_LIMIT_BUCKET_FILES=10
-#export AWS_CODE_DEPLOY_REVISION_DESCRIPTION="Deployed from ${CIRCLE_BRANCH} with build number ${CIRCLE_BUILD_NUM}"
-#export AWS_CODE_DEPLOY_DEPLOYMENT_DESCRIPTION="Deployed via CircleCI on $(date)"
-#export AWS_CODE_DEPLOY_S3_KEY_PREFIX="test-node-deploy"
-#
-#./scripts/aws-code-deploy.sh
+./scripts/aws-code-deploy.sh
